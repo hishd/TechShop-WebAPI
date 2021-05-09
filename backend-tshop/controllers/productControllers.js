547e6dc30c63d22ba64a9e31858ce46bf081b1cf
Hishara Dilshan <hish2k15@gmail.com>
2021-05-09 22:17:18 +0530

    Added delete, create, update, review product endpoints

--- backend-tshop/controllers/productControllers.js
+++ backend-tshop/controllers/productControllers.js
@@ -39,7 +39,117 @@ const getProductByID = asyncHandler(async (req, res) => {
   }
 })
 
-export {
-    getProducts,
-    getProductByID,
+// @desc Delete a product
+// @route DELETE /api/products/:id
+// @access Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    await product.remove()
+    res.json({ message: 'Product removed' })
+  } else {
+    //   res.status(404).json({ message: 'Product not found' })
+    res.status(404)
+    throw new Error('Product not found')
   }
+})
+
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample product',
+    price: 0,
+    user: req.user._id,
+    image: 'images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createdProduct = await product.save()
+
+  res.status(201).json(createdProduct)
+})
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, image, brand, category, countInStock, description } =
+    req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    product.name = name
+    product.price = price
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+    product.description = description
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+// @desc Create a review
+// @route POST /api/products/:id/reviews
+// @access Private
+const reviewProduct = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    const hasReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    )
+    if (hasReviewed) {
+      res.status(400)
+      throw new Error('Product already reviewed!')
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment: comment,
+      user: req.user.id,
+    }
+
+    product.reviews.push(review)
+    product.numReviews = product.reviews.length
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length
+
+    await product.save()
+    res.status(201).json({ message: 'Review added' })
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+// @desc Get top rated products
+// @route GET /api/products/top
+// @access Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+  res.json(products)
+})
+
+export {
+  getProducts,
+  getProductByID,
+  deleteProduct,
+  updateProduct,
+  createProduct,
+  reviewProduct,
+  getTopProducts,
+}
