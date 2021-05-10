d87d875485955e12d010447796ac338643d543c6
Nimesh Lakshan <39796963+shavinimesh@users.noreply.github.com>
2021-05-10 21:08:36 +0530

    added RegisterScreen

---
+++ frontend-tshop/src/Screens/RegisterScreen.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { register } from '../actions/userActions'
+
+const RegisterScreen = ({ location, history }) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState(null)
+
+  const dispatch = useDispatch()
+
+  const userRegister = useSelector((state) => state.userRegister)
+  const { loading, error, userInfo } = userRegister
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  useEffect(() => {
+    //Redirect user to Home if logged in
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      setMessage('Passwords does not match')
+    } else {
+      //Dispatching register action
+      dispatch(register(name, email, password))
+    }
+  }
+
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      {message && <Message variant='danger'>{message}</Message>}
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name' className='form-group'>
+          <Form.Label>Enter name</Form.Label>
+          <Form.Control
+            type='name'
+            placeholder='Enter name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='email' className='form-group'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='password' className='form-group'>
+          <Form.Label>Enter Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='confirmPassword' className='form-group'>
+          <Form.Label>Confirm password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Confirm password'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group className='form-group'>
+          <Button type='submit' variant='primary' className='btn-signin'>
+            Sign Up
+          </Button>
+        </Form.Group>
+      </Form>
+
+      <Row className='py-3'>
+        <Col>
+          Already registered?{' '}
+          <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+            Sign in
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default RegisterScreen
